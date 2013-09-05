85d0f5fc4673e270ae389e910d4a6a8e9b8441f8
Vinayak Shukl <vinayak11118@iiitd.ac.in>
2013-09-05 23:34:50 +0530

    - Halting support for WifiStats for now. All updates shifted to wifitester due to better handling of system broadcast
    - Replaced menu refresh with scan button
    - sorted listview by signal strength
    - modified layout

--- WiFiStatsProject/WiFiStats/src/main/java/com/iiitd/wifistats/WiFiStats.java
+++ WiFiStatsProject/WiFiStats/src/main/java/com/iiitd/wifistats/WiFiStats.java
@@ -6,7 +6,6 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -16,11 +15,14 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WiFiStats extends Activity {
+
 
     WifiManager wifi;
     List<ScanResult> scanResults;
@@ -38,13 +40,13 @@ public class WiFiStats extends Activity {
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         list = new ArrayList<Map<String, String>>();
 
-        if(wifi.isWifiEnabled()==false){
+        if(!wifi.isWifiEnabled()){
             Log.e("DEBUG", "Turning on wifi...");
             Toast.makeText(getApplicationContext(), "Enabling Wifi...",
                     Toast.LENGTH_LONG).show();
             wifi.setWifiEnabled(true);
         } else {
-            Log.e("DEBUG","Wifi is on...");
+            Log.e("DEBUG", "Wifi is on...");
         }
         carryON();
     }
@@ -54,13 +56,19 @@ public class WiFiStats extends Activity {
         scanButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 wifi = (WifiManager) v.getContext().getSystemService(Context.WIFI_SERVICE);
-                if (wifi.startScan() == false) {
+                if (!wifi.startScan()) {
                     Log.e("Error","Scanning could not start");
                 } else {
                     Log.e("DEBUG", "Scanning has started...");
                     Toast.makeText(getApplicationContext(), "Scanning...", Toast.LENGTH_SHORT).show();
                 }
                 scanResults = wifi.getScanResults();
+                Collections.sort(scanResults, new Comparator<ScanResult>() {
+                    @Override
+                    public int compare(ScanResult lhs, ScanResult rhs) {
+                        return (lhs.level > rhs.level ? -1 : (lhs.level == rhs.level ? 0 : 1));
+                    }
+                });
                 list.clear();
                 list = buildData(scanResults);
                 adapter = new SimpleAdapter(v.getContext(), list, R.layout.listitem, new String[]{"BSSID", "strength", "SSID"}, new int[] {R.id.BSSID, R.id.strength, R.id.SSID});
@@ -76,7 +84,6 @@ public class WiFiStats extends Activity {
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
                 if(isChecked){
                     Log.e("DEBUG", "Turning WiFi on...");
                     Toast.makeText(getApplicationContext(), "Turning WiFi on...", Toast.LENGTH_SHORT).show();
