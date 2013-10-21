cb72d35fecc9e2c576c34bc4bcd7f04b3a21e6fc
Prateek <prateek11077@iiitd.ac.in>
2013-10-21 16:33:58 +0530

    Readings and sample files added.

--- DjangoServer/HTTPReceiver/views.py
+++ DjangoServer/HTTPReceiver/views.py
@@ -2,6 +2,7 @@
 
 from django.http import HttpResponse
 import json
+from analyse import get_cluster, load_access_points_academic, euclidean_location
 
 def process(js):
     i=0
@@ -11,6 +12,8 @@ def process(js):
     print "Building: ", js['Location']['Building']
     print "Floor: ",js['Location']['Floor']
     print "ID: ",js['Location']['ID']
+    print "Occupancy", js['Location']['Occupancy']
+    print "Timestamp", js['Time']
 
     for id in js['Readings']:
         print "SSID: ",js['Readings'][id]['SSID']
@@ -22,16 +25,58 @@ def process(js):
     print "Total Readings: ",i
 
 
-def respond(request):
+# def respond(request):
+#     if request.method == 'GET':
+#         html = "<html><body>GET Recevied!</body></html>"
+#
+#     elif request.method == 'POST':
+#         html = "<html><body>You sent a POST message! I got it :) </body></html>"
+#         js = json.loads(request.body)
+#         #print js
+#
+#         path = '/home/prateek/Desktop/Readings.txt'
+#
+#         f = open(path,'a+')
+#         json.dump(js,f)
+#         f.write("\n")
+#         f.close()
+#
+#         process(js)
+#
+#     else:
+#         html = "<html><body>Unknown Method!</body></html>"
+#     return HttpResponse(html)
+
+def query(request):
     if request.method == 'GET':
         html = "<html><body>GET Recevied!</body></html>"
 
     elif request.method == 'POST':
-        html = "<html><body>You sent a POST message! I got it :) </body></html>"
         js = json.loads(request.body)
         #print js
-        process(js)
 
+        res = estimate_location(js)
     else:
         html = "<html><body>Unknown Method!</body></html>"
-    return HttpResponse(html)
+    return HttpResponse(res)
+
+def estimate_location(js):
+    a = {}
+    for access_point in load_access_points_academic():
+        a[access_point] = [0,0]
+
+    for reading in js['Readings']:
+        cluster = get_cluster(js['Readings'][reading]["BSSID"])
+
+        if cluster in a:
+            a[cluster][0] += int(js['Readings'][reading]["Strength"])
+            a[cluster][1] +=1
+    for access_point in a:
+        if (a[access_point][1]==0):
+            a[access_point] = -99
+        else:
+            a[access_point] = a[access_point][0] / a[access_point][1]
+    # print a
+
+    location = euclidean_location(a)
+    return location
