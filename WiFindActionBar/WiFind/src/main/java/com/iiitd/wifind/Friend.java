03fcd6f05f1b0e54c92800046971bd00b604911f
Romil <romil11092@iiitd.ac.in>
2013-12-02 06:35:17 +0530

    Initial Main App Commit

---
+++ WiFindActionBar/WiFind/src/main/java/com/iiitd/wifind/Friend.java
@@ -0,0 +1,14 @@
+package com.iiitd.wifind;
+
+/**
+ * Created by Romil on 19/11/13.
+ */
+public class Friend {
+    public String name;
+    public String location;
+
+    public Friend(String name, String loc){
+        this.name = name;
+        this.location = loc;
+    }
+}
